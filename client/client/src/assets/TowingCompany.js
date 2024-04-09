c512305a1cb59782c9523262bb8a4b8cc633fcd8
Ochieng John Ronnie <johnronnie18@gmail.com>
2024-04-09 11:25:16 +0300

    adding vehicle

--- client/client/src/assets/TowingCompany.js
+++ client/client/src/assets/TowingCompany.js
@@ -1,8 +1,59 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 export default function TowingCompany(){
+    const [car, setCar] = useState({
+        vehicle_id: '',
+        vehicle_plate_number: '',
+        reason_towing: '',
+        location: '',
+        date_of_towing: '',
+        fine_amount: '',
+        towing_company: '',
+        towing_email: '',
+    })
+
+    const handleInput = (e) => {
+        const { name, value } = e.target;
+        setCar({
+            ...car,
+            [name]: value
+        });
+    }
+
+    function handleNewPleadQuery() {
+    const item = {
+        vehicle_id: car.vehicle_id,
+        vehicle_plate_number: car.vehicle_plate_number,
+        reason_towing: car.reason_towing,
+        location: car.location,
+        date_of_towing: car.date_of_towing,
+        fine_amount: car.fine_amount,
+        towing_company: car.towing_company,
+        towing_email: car.towing_email,
+    };
+        fetch("http://127.0.0.1:5000/pleadquery", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(item),
+        })
+        .then((r) => r.json())
+        .then((data) => handleNewPleadQuery(data))
+        setCar({
+            vehicle_id: '',
+            vehicle_plate_number: '',
+            reason_towing: '',
+            location: '',
+            date_of_towing: '',
+            fine_amount: '',
+            towing_company: '',
+            towing_email: '',
+        })
+        alert("New Vehicle successfully added!");
+    }
 
     return(
     <h1>List of our Towed Vehicles</h1>
@@ -11,8 +62,8 @@ export default function TowingCompany(){
     <h2>Fill the below form to enter car details</h2>
     <Form>
       <Form.Group className="mb-3" controlId="formBasicText">
-        <Form.Label>Vehicle</Form.Label>
-        <Form.Control type="text" placeholder="Plate Number" />
+        <Form.Label>Vehicle ID</Form.Label>
+        <Form.Control type="text" placeholder="Vehicle ID" />
         
       </Form.Group>
 
